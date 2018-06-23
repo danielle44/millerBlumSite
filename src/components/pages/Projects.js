761a4d7f2720c46c49015c800363827d113c121c
Danielle Peled <danielle.p@walkme.com>
2018-06-23 22:47:42 +0300

    update mock projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -26,112 +26,41 @@ class Projects extends Component {
                         <div className="portfolio-items">
                             <div className="col-sm-6 col-md-3 col-lg-3 lorem">
                                 <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/01-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div className="hover-bg"> <a href="/img/projects/Jerusalem Biblical Zoo/Jerusalem_Zoo_lake.jpg" title="Project Title" data-lightbox-gallery="gallery1">
                                         <div className="hover-text">
                                             <h4>Lorem Ipsum</h4>
                                         </div>
-                                        <img src="/img/portfolio/01-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
+                                        <img src="/img/projects/Jerusalem Biblical Zoo/Jerusalem_Zoo_lake.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
                                 </div>
                             </div>
                             <div className="col-sm-6 col-md-3 col-lg-3 adipiscing">
                                 <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/02-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div className="hover-bg"> <a href="/img/projects/Keryat Arie - Azorim Park/Keryat Arie - up.jpg" title="Project Title" data-lightbox-gallery="gallery1">
                                         <div className="hover-text">
                                             <h4>Adipiscing Elit</h4>
                                         </div>
-                                        <img src="/img/portfolio/02-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
+                                        <img src="/img/projects/Keryat Arie - Azorim Park/Keryat Arie - up.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
                                 </div>
                             </div>
                             <div className="col-sm-6 col-md-3 col-lg-3 lorem">
                                 <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/03-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div className="hover-bg"> <a href="/img/projects/Kiryat Rabin/kiryat rabin bench.jpg" title="Project Title" data-lightbox-gallery="gallery1">
                                         <div className="hover-text">
                                             <h4>Lorem Ipsum</h4>
                                         </div>
-                                        <img src="/img/portfolio/03-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 lorem">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/04-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Lorem Ipsum</h4>
-                                        </div>
-                                        <img src="/img/portfolio/04-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 adipiscing">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/05-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Adipiscing Elit</h4>
-                                        </div>
-                                        <img src="/img/portfolio/05-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
+                                        <img src="/img/projects/Kiryat Rabin/kiryat rabin bench.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
                                 </div>
                             </div>
                             <div className="col-sm-6 col-md-3 col-lg-3 dolor">
                                 <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/06-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                    <div className="hover-bg"> <a href="/img/projects/Yavne G/yavneG1.jpg" title="Project Title" data-lightbox-gallery="gallery1">
                                         <div className="hover-text">
                                             <h4>Dolor Sit</h4>
                                         </div>
-                                        <img src="/img/portfolio/06-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
+                                        <img src="/img/projects/Yavne G/yavneG1.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
                                 </div>
                             </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 dolor">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/07-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Dolor Sit</h4>
-                                        </div>
-                                        <img src="/img/portfolio/07-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 lorem">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/08-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Lorem Ipsum</h4>
-                                        </div>
-                                        <img src="/img/portfolio/08-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 adipiscing">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/09-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Adipiscing Elit</h4>
-                                        </div>
-                                        <img src="/img/portfolio/09-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 dolor">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/10-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Dolor Sit</h4>
-                                        </div>
-                                        <img src="/img/portfolio/10-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 dolor">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/11-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Dolor Sit</h4>
-                                        </div>
-                                        <img src="/img/portfolio/11-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-md-3 col-lg-3 lorem">
-                                <div className="portfolio-item">
-                                    <div className="hover-bg"> <a href="/img/portfolio/12-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
-                                        <div className="hover-text">
-                                            <h4>Lorem Ipsum</h4>
-                                        </div>
-                                        <img src="/img/portfolio/12-small.jpg" className="img-responsive" alt="Project Title"/> </a> </div>
-                                </div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
