1e24f7c35283fbfee16e3e679fc8410e10676213
Danielle Peled <danielle.p@walkme.com>
2018-07-28 13:46:12 +0300

    add my grid

--- src/components/pages/projects/Projects_old.js
+++ src/components/pages/projects/Projects_old.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ReactGridLayout from 'react-grid-layout';
-import _ from 'underscore';
 import {projectsLightData} from '../../../mocks/projectsLight';
 import {projectCategoriesData} from '../../../mocks/projectCategories';
 import ProjectItem from './projecttem';
@@ -34,9 +32,9 @@ class Projects extends Component {
     createFilters() {
         let filters = projectCategoriesData
             .map((category) => ({
-                title: category.title,
-                filter: `.${category.id}`
-            }));
+            title: category.title,
+            filter: `.${category.id}`
+        }));
 
         filters = this.addEmptyFilter(filters);
         return filters.map(this.createFilterItem);
